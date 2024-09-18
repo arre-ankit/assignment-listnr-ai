7e1043c67960f8b887dbc4165b4f03a1b8effe08
arre_ankit <arre.ankit@gmail.com>
2024-09-19 03:41:02 +0530

    background

--- app/component/Hero-Left.tsx
+++ app/component/Hero-Left.tsx
@@ -3,30 +3,24 @@ import Link from "next/link";
 export default function HeroLeft() {
   return (
     <section className=" items-start gap-[24px] w-[538px] h-[348px]">
-      {/* Main Text Section */}
       <div className="flex flex-col items-start gap-[44px]  w-[538px] h-[348px]">
         <div className="flex flex-col items-start gap-[24px] w-[538px] h-[260px]">
-          {/* Title */}
           <h1 className="text-[64px] font-semibold leading-[100%] tracking-[-0.04em] text-[#353C44] w-[538px] h-[192px]">
             Realistic Voiceovers In Seconds
           </h1>
 
-          {/* Subtext */}
           <p className="text-[18px] font-normal leading-[22px] text-[#5E5E5E] w-[538px] h-[44px]">
             Listnr lets you create voiceovers with 1000+ different voices in over 500 languages, including a clone of your voice.
           </p>
         </div>
 
-        {/* Buttons Section */}
         <div className="flex flex-row items-start gap-[17px] w-[395px] h-[44px]">
-          {/* Get Started Button */}
           <div className="flex justify-center items-center px-[24px] py-[16px] gap-[8px] w-[199px] h-[44px] bg-[#353C44] rounded-[12px]">
             <Link href="#" className="text-[16px] font-semibold leading-[19px] text-white w-[151px] h-[19px]">
               Get Started for free
             </Link>
           </div>
 
-          {/* Clone your Voice Button */}
           <div className="flex justify-center items-center px-[24px] gap-[8px] w-[179px] h-[44px] bg-white rounded-[12px]">
             <Link href="#" className="text-[16px] font-semibold leading-[19px] text-[#5E5E5E] w-[131px] h-[19px] whitespace-nowrap">
                 Clone your Voice
--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,38 @@ import Navbar from "./component/Navbar";
 export default function Home() {
   return (
     <main className="relative w-[1440px] h-[872px]">
+
       <div
-          className="absolute -z-10"
+          className="absolute -z-20"
           style={{
-            width: "1775.69px",
-            height: "887.84px",
-            left: "-200.39px",
-            top: "726.13px",
-            background: "linear-gradient(281.1deg, #657EFF 200%, #076AC6 400%)",
-            mixBlendMode: "soft-light",
-            filter: "blur(600px)",
+            width: "1775px",
+            height: "1000px",
+            top: "76px",
+            left: "-200px",
+            gap: "20px",
             transform: "rotate(-38.9deg)",
-          }}>
+            mixBlendMode: "saturation",
+            filter: "blur(200px)",
+          }}
+        >
+          <Image src="/Background.png" alt="SVG Image" fill  />
       </div>
-      
+
       <div
           className="absolute -z-20"
           style={{
             width: "1505px",
             height: "1630px",
-            top: "-408px",
+            top: "-208px",
             left: "-21px",
             gap: "20px",
-            mixBlendMode: "normal",
+            mixBlendMode: "overlay",
+            
           }}
         >
-          <Image src="/Frame 455.svg" alt="SVG Image" layout="fill" />
+          <Image src="/Frame 455.svg" alt="SVG Image" fill />
       </div>
       
-
       <Navbar />
       <Hero />
     </main>
